cf6a44fb0a852d5d34b9b57a1e6fd72198c8f78a
Ian Page Hands <iphands@gmail.com>
2020-10-30 21:55:32 -0400

    strip bins

--- helper.sh
+++ helper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-su iphands -c "RUSTFLAGS='-C target-cpu=znver2' cargo build --release"
+su iphands -c "RUSTFLAGS='-C target-cpu=znver2' cargo build --release && \
+cp ./target/release/niced ./target/release/niced.nostrip && \
+strip -s ./target/release/niced"
 ls -lh ./target/release/
 time ./target/release/niced
